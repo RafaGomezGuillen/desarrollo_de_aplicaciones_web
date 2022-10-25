8194f0535f7625c9e2e351428ce22ad48d37798f
rafa <gomezra72.a@gmail.com>
2022-10-25 12:39:32 +0200

    pro

---
+++ programacion/ejercicios_programacion/UnionDeCadena/Programa03.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UnionDeCadena {
+  class Programa03 {
+    public static void Main(String[] args) {
+      Console.WriteLine("Solicitar dos mensajes solicitados por teclado y mostrar la union de ambas cadenas.");
+
+      Console.Write("Introduce la primera cadena: ");
+      string cadena_01 = Console.ReadLine();
+
+      Console.Write("Introduce la segunda cadena: ");
+      string cadena_02 = Console.ReadLine();
+
+      Console.WriteLine("Union: " + cadena_01 + " " + cadena_02);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ programacion/ejercicios_programacion/my_proyect/Programa01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumaDeDosNumeros {
+  class Programa01 {
+    public static void Main(String[] args) {
+      Console.WriteLine("Suma de dos numeros solicitados por teclado.");
+
+      Console.Write("Introduce el primer numero: ");
+      int numero_01 = Convert.ToInt32(Console.ReadLine());
+
+      Console.Write("Introduce el segundo numero: ");
+      int numero_02 = Convert.ToInt32(Console.ReadLine());
+
+      int suma = numero_01 + numero_02;
+
+      Console.WriteLine("La suma de " + numero_01 + " + " + numero_02 + " = " + suma);
+      Console.ReadKey();
+    }
+  }
+}
