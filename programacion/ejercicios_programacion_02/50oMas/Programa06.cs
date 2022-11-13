4947e72954fcc264853aa054cc88dc12fef63ffa
Rafa PC <gomezra72.a@gmail.com>
2022-11-13 15:26:36 +0000

    s

--- programacion/ejercicios_programacion_02/50oMas/Programa06.cs
+++ programacion/ejercicios_programacion_02/50oMas/Programa06.cs
@@ -4,20 +4,16 @@ namespace Suma50oMas {
   class Programa06 {
     public static void Main(string[] args) {
       Console.Write("Pide números por teclado, hasta que sumen 50 o más. Introduce tu número: ");
-      string? cadena_01 = Console.ReadLine();
-      if (int.TryParse(cadena_01, out int numero_01)) {
-        numero_01 = Convert.ToInt32(Console.ReadLine());
-        while (numero_01 <= 50) {
-          Console.WriteLine("Suma = " + numero_01);
-          Console.Write("Introduce tu número se para hasta que su suma sea 50 o más: ");
-        }
-        numero_01 = numero_01 + numero_01;
-      }
-      else {
-        Console.WriteLine(cadena_01 + " no es número");
-      }
+      int suma = 0, numero_01 = 0;
 
+      while (suma <= 50) {
+          Console.Write("Introduce tu número se para hasta que su suma sea 50 o más: ");
+          numero_01 = Convert.ToInt32(Console.ReadLine());
+          suma = suma + numero_01;
+          Console.WriteLine("Suma = " + suma);
+      }
       Console.ReadKey();
     }
   }
 }
+
