f70c01a0355b5091ad69da0cb655be37702dc244
Rafa Gomez <gomezra72.a@gmail.com>
2022-11-22 14:50:39 +0000

    22/11/2022

---
+++ programacion/ejercicios_programacion_05/Programa01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programa01 {
+  class Program {
+    public static void Main(string[] args) {
+      Console.WriteLine("Dada una cadena de caracteres introducida por teclado, " +
+                        "contar el número de caracteres que la componen.");
+      Console.Write("Introduce la cadena: ");
+      string? cadena_01 = Console.ReadLine();
+      Console.WriteLine("El número de carácteres de " + cadena_01 + " es " + cadena_01.Length);
+    }
+  }
+}
---
+++ programacion/ejercicios_programacion_05/Programa02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Programa02 {
+  class Program {
+    public static void Main(string[] args) {
+      Console.WriteLine("Solicitada una frase y una letra, encontrar el número " +
+                        "de veces que dicha letra aparece en la frase dada.");
+
+      Console.Write("Introduce la cadena: ");
+      string? cadena_01 = Console.ReadLine();
+
+      Console.Write("Introduce una letra: ");
+      char letra_01 = Convert.ToChar(Console.ReadLine());
+
+      int contador_01 = 0;
+      for (int i = 0; i < cadena_01.Length; i++) {
+        if (cadena_01.Contains(letra_01)) {
+          contador_01++;
+        }
+      }
+      Console.WriteLine(letra_01 + " aparece " + contador_01 + " veces en la cadena " + cadena_01);
+    }
+  }
+}
