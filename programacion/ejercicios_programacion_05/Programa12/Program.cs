afe8352ec236954de1f3343f873d308ae736d0f2
Rafa PC <gomezra72.a@gmail.com>
2022-11-24 17:19:15 +0000

    24/11/2022

--- programacion/ejercicios_programacion_05/Programa12/Program.cs
+++ programacion/ejercicios_programacion_05/Programa12/Program.cs
@@ -6,8 +6,18 @@ namespace Programa12 {
       Console.WriteLine("Desarrolla un programa que devuelva una cadena con un carácter repetido n veces, siendo n solicitado por teclado.");
       Console.Write("Introduce un caracter: ");
       char caracter_01;
+      int numero_01;
       while (!char.TryParse(Console.ReadLine(), out caracter_01)) {
+        Console.Write("ERROR. No ha introducido un caracter. Introduce un caracter: ");
+      }
+
+      Console.Write("Introduce un número: ");
+      while (!int.TryParse(Console.ReadLine(), out numero_01)) {
         Console.Write("ERROR. No ha introducido un número. Introduce un número: ");
+      }
+
+      for (int i = 1; i <= numero_01; i++) {
+        Console.WriteLine(i + ": " + caracter_01);
       }
     }
   }
