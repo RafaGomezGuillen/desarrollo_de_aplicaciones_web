21054da807ae21d4d79f4382289d63b3d28378b8
Rafa Gómez <gomezra72.a@gmail.com>
2022-11-25 15:51:56 +0000

    Add files via upload

---
+++ programacion/ejercicios_programacion_05/Programa13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Programa13 {
+  class Program {
+    public static void Main(string[] args) {
+      Console.WriteLine("Programa que, dada una cadena de caracteres, elimine los caracteres " +
+                        "en blanco a la izquierda de una cadena y la muestre por pantalla.");
+
+      Console.Write("Introduce una frase: ");
+      string? cadena_01 = Console.ReadLine();
+
+      string cadena_02 = "";
+      for (int i = 0; i < cadena_01.Length; i++) {
+        if (cadena_01[i] != ' ') {
+          cadena_02 += cadena_01[i];
+        }
+      }
+      Console.WriteLine(cadena_02);
+    }
+  }
+}
